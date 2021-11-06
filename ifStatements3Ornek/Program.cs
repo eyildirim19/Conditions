f100bf874a72558182c33a7c12e9fd0a0f2348b6
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-11-06 11:48:59 +0300

    basic hesap makinesi

---
+++ ifStatements3Ornek/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ifStatements3Ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Sayıyı Giriniz....");
+            int sayi1 = Convert.ToInt32(Console.ReadLine()); // stringi int'e dönüştürdük...
+
+            Console.WriteLine("2. Sayıyı Giriniz");
+            int sayi2 = Convert.ToInt32(Console.ReadLine()); // string int'e dönüştürdük..
+
+            int sonuc;
+
+            sonuc = sayi1 + sayi2;
+            //Console.WriteLine("Girilen Sayıların Toplamı " + sonuc);
+            //Console.WriteLine(sayi1 + " Sayısı ile " + sayi2 + " Sayısının toplamı = " + sonuc);
+            Console.WriteLine($"{sayi1} Sayısı ile {sayi2} sayısının toplamı = {sonuc}");
+            //$ => stringi formatlamak için kullanılır. string içerisine { } ile dışarıdan parametre vermiş oluruz...
+
+            sonuc = sayi1 * sayi2;
+            // Console.WriteLine("Girilen Sayıların Çarpımı " + sonuc); // Girilen Sayıların Çarpımı 2
+            // formatlı yazalım...
+            Console.WriteLine($"{sayi1} sayı ile {sayi2} sayısının çarpımı = {sonuc}");
+
+            sonuc = sayi1 / sayi2;
+            Console.WriteLine($"{sayi1} sayısı ile {sayi2} sayısının bölümü = {sonuc}");
+
+            Console.ReadKey(); // beklemeye aldık...
+        }
+    }
+}
---
+++ ifStatements4Ornek/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ifStatements4Ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi1, sayi2, sonuc = 0;
+            char islem;
+
+            Console.WriteLine("1. Sayıyı giriniz");
+            sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("2. Sayıyı giriniz");
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Yapmak istediğiniz işlemi seçiniz (+, -, *, /)");
+            islem = Convert.ToChar(Console.ReadLine());
+
+            if (islem == '+')
+            {
+                sonuc = sayi1 + sayi2;
+            }
+            else if (islem == '*')
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            else if (islem == '/')
+            {
+                sonuc = sayi1 / sayi2;
+            }
+            else if (islem == '-')
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz İşlem");
+            }
+
+            Console.WriteLine($"işlem sonucunuz = {sonuc}");
+            Console.WriteLine("Kapatmak için bir tuşa basınız");
+            Console.ReadKey();
+        }
+    }
+}
